3a1a62b9afe1b9b7dad4ccf680edcb8508fbde6a
Alex Welcing <alexwelcing@gmail.com>
2019-01-09 17:34:34 -0500

    Update to use correct site.

--- src/website-config.ts
+++ src/website-config.ts
@@ -29,17 +29,17 @@ export interface WebsiteConfig {
 }
 
 const config: WebsiteConfig = {
-  title: 'Ghost',
-  description: 'The professional publishing platform',
+  title: 'Alex Welcing 2019',
+  description: 'Business and software developmeent',
   coverImage: 'img/blog-cover.jpg',
   logo: 'img/ghost-logo.png',
   lang: 'en',
-  siteUrl: 'https://gatsby-casper.netlify.com',
-  facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
+  siteUrl: 'https://atrispina.com',
+  facebook: 'https://www.facebook.com/alexwelcing',
+  twitter: 'https://twitter.com/alexwelcing',
   showSubscribe: true,
-  mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
-  mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
+  mailchimpAction: 'TBD',
+  mailchimpName: 'TBD',
 };
 
 export default config;
